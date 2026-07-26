da0372e0290c5848ec540dc0e6ed4260c50e31b1
Christi Maria Biju <christimariabiju@gmail.com>
2026-07-26 19:01:10 +0530

    Add removeElement method to remove specified values
    Implement a solution to remove specified elements from an array and return the new length.

---
+++ Two pointers/27_Remove Elemennt.py
@@ -0,0 +1,23 @@
+class Solution:
+    def removeElement(self, nums, val):
+
+        write = 0
+
+        for read in range(len(nums)):
+
+            if nums[read] != val:
+
+                nums[write], nums[read] = nums[read], nums[write]
+
+                write += 1
+
+        return write
+      """
+In both problems, the write pointer has the same meaning:
+
+"The next position where a valid element should be placed."
+
+The only difference is what the problem asks us to do with it.
+
+Move Zeroes: Use write to rearrange the array. No return.
+Remove Element: Use write to rearrange the array and return its final value because it represents k. Return write.
